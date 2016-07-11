7e23c676ce39d2534dae915898159fd17d4222b1
jjacobson <jjacobson@catalystitservices.com>
2016-07-11 14:36:07 -0700

    Changed ScrollableCheckboxGroup to a dumb component

--- src/components/common/ScrollableCheckboxGroup.jsx
+++ src/components/common/ScrollableCheckboxGroup.jsx
@@ -1,20 +1,25 @@
 import React, {propTypes} from 'react';
 
-class ScrollableCheckboxGroup extends React.Component {
-
-
-    render() {
-        return(
-            <div>
-
-            </div>
-        );
-    }
+const scrollStyle = {
+    width: '300px',
+    border: '2px solid'
 }
+
+const ScrollableCheckboxGroup = ({options, onClick}) => {
+    return(
+        <div style={scrollStyle}>
+            {
+                this.options.map((option, index) => {
+                    <Checkbox key={option.id} value={option.id} name={option.name} />
+                })
+            }
+        </div>
+    );
+};
 
 ScrollableCheckboxGroup.propTypes = {
     options: PropTypes.array.isRequired,
     onClick: PropTypes.func.isRequired
-}
+};
 
 export default ScrollableCheckboxGroup;
