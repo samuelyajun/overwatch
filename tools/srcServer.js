10a4f8316d2eb6c1a8afd44a44467e2fe6105d6c
jjacobson <jjacobson@catalystitservices.com>
2016-07-15 10:10:49 -0700

    Added reverse proxy to express server for schedule service

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -3,6 +3,7 @@ import webpack from 'webpack';
 import path from 'path';
 import config from '../webpack.config.dev';
 import open from 'open';
+import proxy from 'http-proxy-middleware';
 
 /* eslint-disable no-console */
 
@@ -10,12 +11,23 @@ const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
+//for now this is just for scheduling
+const options = {
+    target: 'http://localhost:8090',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/schedule': '/' //remove /api from url before making request
+    }
+};
+
 app.use(require('webpack-dev-middleware')(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
+
+app.use('/schedule', proxy(options));
 
 app.get('*', function(req, res) {
     res.sendFile(path.join( __dirname, '../src/index.html'));
