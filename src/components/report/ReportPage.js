c7a904cb44200d5a096c0df3e7bca14e18005d2c
jblair <Jblair@catalystdevworks.com>
2016-07-15 10:46:04 -0700

    table lay out

--- src/components/report/ReportPage.js
+++ src/components/report/ReportPage.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import Header from '../common/Header';
+import Button from '../common/Button';
 
 const reportOuterDivStyle = {
     marginTop: '75px'
@@ -8,37 +9,37 @@ const reportOuterDivStyle = {
 const reportsLink='http://localhost:8040/getSurveys';
 
 class ReportPage extends React.Component {
-    construner (props,context){
-        this.pror
-    }
+
+
     render() {
+
         return (
-            <div style={reportOuterDivStyle}>
+            <div className="container-fluid" style={reportOuterDivStyle}>
                 <div>
                 <h1>Report Page</h1>
                 <a href={reportsLink} className="btn btn-info" role="button">Generate New Report</a>
                 </div>
                 <table className="table">
                     <thead>
-                    <tr>
-                        <th>Report Generated Date</th>
-                        <th>Survey Name</th>
-                        <th>Group</th>
-                        <th>Report</th>
+                        <tr>
+                            <th>Report Generated Date</th>
+                            <th>Survey Name</th>
+                            <th>Group</th>
+                            <th>Report</th>
 
-                    </tr>
+                        </tr>
                     </thead>
                     <tbody>
-                    <div>{ somearray.map((object)=> {
-                            return (<div class="row">
-                                <div class="col-xs-12 col-md-8">.col-xs-12 .col-md-8</div>
-                                <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-                            </div>)
-                        }
-                    )
-                    }
-
-                    </div>
+                        <tr>
+                            <td>sd</td>
+                            <td>sd</td>
+                            <td>
+                                <Button className={"btn btn-info"} type={"button"} label={"Generate Excel"}/>
+                            </td>
+                            <td>
+                                <Button className={"btn btn-info"} type={"button"} label={"Generate CSV"}/>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
