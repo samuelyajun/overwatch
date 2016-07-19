2474b0ae334fa4f705da8654d4b42a2829ad8f9f
jjacobson <jjacobson@catalystitservices.com>
2016-07-19 16:03:16 -0700

    Added ability to get users from the backend

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,5 +1,6 @@
 import * as types from './actionTypes';
-import UserApi from '../api/mockUserApi';
+// import UserApi from '../api/mockUserApi';
+import ObjectParser from '../utils/objectParser';
 
 export function loadUsersSuccess(users) {
     return {type: types.LOAD_USERS_SUCCESS, users};
@@ -7,8 +8,12 @@ export function loadUsersSuccess(users) {
 
 export function loadUsers() {
     return function(dispatch) {
-        return UserApi.getAllUsers().then((users) => {
-            dispatch(loadUsersSuccess(users));
+        return fetch('/schedule/users').then((response) => {
+            response.json().then((json) => {
+                let userArray = ObjectParser.getObjects(json);
+                console.log(userArray);
+                dispatch(loadUsersSuccess(userArray));
+            });
         }).catch((error) => {
             throw(error);
         });
