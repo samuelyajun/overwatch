4897a68acecf6de4d4f37378efadda3580ec066a
Alexandra Moss <amoss@catalystdevworks.com>
2016-07-22 10:29:23 -0700

    added dependencies for node, continued logic for error/success message component.

--- src/components/survey/SurveyResponseForm.js
+++ src/components/survey/SurveyResponseForm.js
@@ -9,7 +9,7 @@ const surveyPageOuterDiv = {
 };
 
 
-const SurveyResponseForm = ({ survey, onSubmit }) => {
+const SurveyResponseForm = ({ survey, onSubmit, className }) => {
 
     let submitButtonType = 'submit';
     let cancelButtonType = 'reset';
@@ -17,7 +17,7 @@ const SurveyResponseForm = ({ survey, onSubmit }) => {
     let cancelButtonClass = 'btn btn-default';
 
     return (
-        <div style={surveyPageOuterDiv} className="container">
+        <div style={surveyPageOuterDiv} className={className + " container"}>
             <form name="surveyForm" noValidate>
                     <div className="table-responsive">
                         {survey.template.type === "Qualitative" ?
--- src/components/survey/SurveyResponsePage.js
+++ src/components/survey/SurveyResponsePage.js
@@ -18,13 +18,14 @@ class SurveyResponsePage extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            surveySubmitted: false,
+            showError: false,
+            showConfirmation: false,
             showSurveyForm: true
         };
     }
 
     onSubmit() {
-        this.setState({surveySubmitted: true});
+        this.setState({showConfirmation: true});
         this.setState({showSurveyForm: false});
     }
 
@@ -53,14 +54,14 @@ class SurveyResponsePage extends React.Component {
             <div style={surveyContainer}>
                 {surveys.length > 0 ?
                     <div>
-                        <SurveyResponsePageHeader
+                        <SurveyResponsePageHeader 
                             headerTitle={surveys[i].template.name + ' Survey'}
                         />
-                        <SurveyResponseForm survey={surveys[i]} onSubmit={this.onSubmit}/>
+                        <SurveyResponseForm className={this.state.showSurveyForm ? '' : 'hidden'} survey={surveys[i]} onSubmit={this.onSubmit}/>
                     </div>
-                    : <MessageComponent title={'Survey Not Found'}/>
+                    :  null
                 }
-                <MessageComponent title={'Survey Submitted!'} text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fermentum semper mollis. Etiam leo nunc, hendrerit vitae mauris vitae, eleifend suscipit mi. Suspendisse potenti. Quisque vitae maximus enim. '} />
+                <MessageComponent className={this.state.showConfirmation ? '' : 'hidden'} title={'Survey Submitted!'} text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fermentum semper mollis. Etiam leo nunc, hendrerit vitae mauris vitae, eleifend suscipit mi. Suspendisse potenti. Quisque vitae maximus enim. '} />
             </div>
         );
     }
