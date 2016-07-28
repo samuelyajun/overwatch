e6fb9613a772f6cc33b9e714544f94e9aa482a87
Katy LaFrance <klafrance@catalystdevworks.com>
2016-07-28 16:02:29 -0700

    added questionLink and value to handleNumericChange function

--- src/components/survey/SurveyResponsePage.js
+++ src/components/survey/SurveyResponsePage.js
@@ -15,6 +15,8 @@ const surveyContainer = {
 
 class SurveyResponsePage extends React.Component {
 
+
+
     constructor(props, context) {
         super(props, context);
         this.state = {
@@ -22,15 +24,17 @@ class SurveyResponsePage extends React.Component {
             showConfirmation: false,
             showSurveyForm: true
         };
+        const answers = [];
         this.onSubmit = this.onSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleNumericChange = this.handleNumericChange.bind(this);
     }
 
     onSubmit(event) {
+        console.log("onSubmit reached");
         event.preventDefault();
         if(this.validateForm() === true){
-            saveSurvey(event);
+            console.log(event);
             this.setState({showConfirmation: !this.state.showConfirmation});
             this.setState({showSurveyForm: !this.state.showSurveyForm});
         }
@@ -41,9 +45,11 @@ class SurveyResponsePage extends React.Component {
         let errors = Object.assign({},this.state.errors);
         let isValid = true;
 
+        const answers = [];
         const {query} = this.props.location;
         let i = query.surveyId;
         const {surveys} = this.props;
+
 
         surveys[i].template.questions.map(
             (question, index) => {
@@ -68,7 +74,17 @@ class SurveyResponsePage extends React.Component {
                     toastr.error('Question ' + ++index +' is missing a response');
 
                     isValid = false;
-                    return;
+
+                }
+                else {
+                    surveysCopy[i].template.questions[index].value = event.target.value;
+                    let questionId = surveysCopy[i].template.questions[index].id;
+
+                    let answerValue = event.target.value;
+                    let questionLink = "/question/" + questionId;
+
+                    answers.push(questionLink, answerValue);
+                    console.log(answers);
                 }
             }
         );
@@ -76,28 +92,45 @@ class SurveyResponsePage extends React.Component {
         this.setState({errors});
         return isValid;
     }
+
     // Handles likert question responses
     handleChange(value, event) {
         const {query} = this.props.location;
         let i = query.surveyId;
+        console.log("Survey ID: " + i);
+        let questionLink = "/question/"+ i ;
         const {surveys} = this.props;
         const index = event.target.name;
 
+        console.log("Location" + this.props.location);
         let surveysCopy = Object.assign ({}, surveys);
         surveysCopy[i].template.questions[index].selectedValue = value;
+        let answerValue = value;
+        console.log("answerValue: " + answerValue);
         this.setState({surveys});
+
     }
 
     // Handles numeric question responses
     handleNumericChange(event) {
+
         const {query} = this.props.location;
         let i = query.surveyId;
         const {surveys} = this.props;
         const index = event.target.name;
-
         let surveysCopy = Object.assign ({}, surveys);
+
         surveysCopy[i].template.questions[index].value = event.target.value;
+        let questionId = surveysCopy[i].template.questions[index].id;
+
+        let answerValue = event.target.value;
+        let questionLink = "/question/" + questionId;
+
+        answers.push(questionLink, answerValue);
+        console.log(answers);
+
         this.setState({surveys});
+
     }
 
     render() {
