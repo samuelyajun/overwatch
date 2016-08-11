a0e1b93fbf6e6d3a88117a7b970352a8e83c3f18
Michael Derfler <mderfler@LT10372.catalystsolves.com>
2016-08-11 09:18:01 -0700

    separated attributes into dumb component

---
+++ src/components/schedule/AttributesComponent.jsx
@@ -0,0 +1,115 @@
+import React, {PropTypes} from 'react';
+import SelectInput from '../common/SelectInput.jsx';
+import TextInput from '../common/TextInput';
+
+const AttributesComponent = ({allowedAttributesZero, allowedAttributesOne, allowedAttributesTwo, onUpdateAttribute}) => {
+
+    return (
+      <div className="row">
+
+       <div className="col-md-6">
+           <ul className="list-unstyled">
+               <li>
+                   <SelectInput
+                       name="CLIENT"
+                       label="Client"
+                       defaultOption="-choose-"
+                       defaultOptionValue=""
+                       defaultOptionLabel="--Select Client--"
+                       value={allowedAttributesZero}
+                       onChange={onUpdateAttribute}
+                       options={[
+                          {
+                                id:"http://localhost:8090/allowedAttributes/7",
+                                text: "Cake Systems",
+                                value: "http://localhost:8090/allowedAttributes/7"
+                            },
+                             {
+                                id:"http://localhost:8090/allowedAttributes/8",
+                                text: "Cockram",
+                                value: "http://localhost:8090/allowedAttributes/8"
+                            },
+                           {
+                                id:"http://localhost:8090/allowedAttributes/9",
+                                text: 'Catalyst DevWorks',
+                                value: "http://localhost:8090/allowedAttributes/9"
+                            },
+                            {
+                                id:"http://localhost:8090/allowedAttributes/10",
+                                text: "Cambia",
+                                value: "http://localhost:8090/allowedAttributes/10"
+                            }
+                        ]}
+                       icon="glyphicon glyphicon-user"
+                   />
+               </li>
+               <li>
+                   <SelectInput
+                       name="PROJECT"
+                       label="Project"
+                       defaultOptionValue=""
+                       defaultOptionLabel="--Select Project--"
+                       value={allowedAttributesOne}
+                       onChange={onUpdateAttribute}
+                       options={[
+                           {
+                                id:"http://localhost:8090/allowedAttributes/11",
+                                text: "Cake Systems",
+                                value: "http://localhost:8090/allowedAttributes/11"
+                            },
+                             {
+                                id:"http://localhost:8090/allowedAttributes/12",
+                                text: "3DS MAC and AutoCAD",
+                                value: "http://localhost:8090/allowedAttributes/12"
+                            },
+                              {
+                                id:"http://localhost:8090/allowedAttributes/13",
+                                text: 'Overwatch',
+                                value: "http://localhost:8090/allowedAttributes/13"
+                            },
+                            {
+                                id:"http://localhost:8090/allowedAttributes/14",
+                                text: "Mobile",
+                                value: "http://localhost:8090/allowedAttributes/14"
+                            }
+                        ]}
+                       icon="glyphicon glyphicon-briefcase"
+                   />
+               </li>
+           </ul>
+       </div>
+       <div className="col-md-6">
+           <ul className="list-unstyled">
+               <li>
+                    <SelectInput
+                        name="OFFICE"
+                        label="Office"
+                        defaultOptionValue=""
+                        defaultOptionLabel="--Select Location--"
+                        value={allowedAttributesTwo}
+                        onChange={onUpdateAttribute}
+                        options={[
+                            {
+                                id:"http://localhost:8090/allowedAttributes/5",
+                                text: 'Beaverton',
+                                value: "http://localhost:8090/allowedAttributes/5"
+                            },
+                            {
+                                id:"http://localhost:8090/allowedAttributes/6",
+                                text: "Baltimore",
+                                value: "http://localhost:8090/allowedAttributes/6"
+                            }
+                             ]}
+                        icon="glyphicon glyphicon-globe"
+                    />
+               </li>
+           </ul>
+       </div>
+
+    </div>
+			
+    );
+
+};
+
+export default AttributesComponent;
--- src/components/schedule/ScheduleForm.jsx
+++ src/components/schedule/ScheduleForm.jsx
@@ -6,6 +6,7 @@ import UserCheckboxGroup from './UserCheckboxGroup.jsx';
 import RespondentList from './RespondentList.jsx';
 import UserForm from './UserSelection.jsx';
 import SurveyScheduleDate from './SurveyScheduleDate.jsx';
+import AttributesComponent from './AttributesComponent.jsx';
 import Button from '../common/Button.jsx';
 //redux imports
 import * as userActions from '../../actions/userActions';
@@ -292,124 +293,31 @@ class ScheduleForm extends React.Component {
                     <div className="row">
                         <div className="panel">
                             <div className="panel-heading"  style={schedulePanel}><h4>1. General Information</h4></div>
-                            <div className="panel-body"> 
+                              <div className="panel-body"> 
 
-                              <div className="col-xs-6">
+                                  <div className="col-xs-6">
 
-                                <SurveyScheduleDate templateUri={this.state.schedule.templateUri} onUpdateTemplate={this.onUpdateTemplate}
-                                  templateOptions={templateOptions} errorsTemplateUri={this.state.errors.templateUri.required}
-                                  scheduleFrequency={this.state.schedule.frequency} onUpdate={this.onUpdate} scheduleStartDate={this.state.schedule.startDate}
-                                  validateStartDate={this.validateStartDate} errorsStartDate={this.state.errors.startDate.required}
-                                  scheduleEndDate={this.state.schedule.endDate} validateEndDate={this.validateEndDate}
-                                  validateEndDate={this.state.errors.endDate.afterStart}
-                                />
+                                    <SurveyScheduleDate templateUri={this.state.schedule.templateUri} onUpdateTemplate={this.onUpdateTemplate}
+                                      templateOptions={templateOptions} errorsTemplateUri={this.state.errors.templateUri.required}
+                                      scheduleFrequency={this.state.schedule.frequency} onUpdate={this.onUpdate} scheduleStartDate={this.state.schedule.startDate}
+                                      validateStartDate={this.validateStartDate} errorsStartDate={this.state.errors.startDate.required}
+                                      scheduleEndDate={this.state.schedule.endDate} validateEndDate={this.validateEndDate}
+                                      validateEndDate={this.state.errors.endDate.afterStart}
+                                    />
+
+                                  </div>
+
+                                  <div className="col-xs-6 well">
+                                    <AttributesComponent allowedAttributesZero={this.state.allowedAttributes[0].attributeValue}
+                                      allowedAttributesOne={this.state.allowedAttributes[1].attributeValue}
+                                      allowedAttributesTwo={this.state.allowedAttributes[2].attributeValue}
+                                      onUpdateAttribute={this.onUpdateAttribute}
+                                     />
+                                    
+                                  </div>
 
                               </div>
-                              
-                                   <div className="col-xs-6 well">
-                                       <div className="row">
-                                       <div className="col-md-6">
-                                           <ul className="list-unstyled">
-                                               <li>
-                                                   <SelectInput
-                                                       name="CLIENT"
-                                                       label="Client"
-                                                       defaultOption="-choose-"
-                                                       defaultOptionValue=""
-                                                       defaultOptionLabel="--Select Client--"
-                                                       value={this.state.allowedAttributes[0].attributeValue}
-                                                       onChange={this.onUpdateAttribute}
-                                                       options={[
-                                                          {
-                                                                id:"http://localhost:8090/allowedAttributes/7",
-                                                                text: "Cake Systems",
-                                                                value: "http://localhost:8090/allowedAttributes/7"
-                                                            },
-                                                             {
-                                                                id:"http://localhost:8090/allowedAttributes/8",
-                                                                text: "Cockram",
-                                                                value: "http://localhost:8090/allowedAttributes/8"
-                                                            },
-                                                           {
-                                                                id:"http://localhost:8090/allowedAttributes/9",
-                                                                text: 'Catalyst DevWorks',
-                                                                value: "http://localhost:8090/allowedAttributes/9"
-                                                            },
-                                                            {
-                                                                id:"http://localhost:8090/allowedAttributes/10",
-                                                                text: "Cambia",
-                                                                value: "http://localhost:8090/allowedAttributes/10"
-                                                            }
-                                                        ]}
-                                                       icon="glyphicon glyphicon-user"
-                                                   />
-                                               </li>
-                                               <li>
-                                                   <SelectInput
-                                                       name="PROJECT"
-                                                       label="Project"
-                                                       defaultOptionValue=""
-                                                       defaultOptionLabel="--Select Project--"
-                                                       value={this.state.allowedAttributes[1].attributeValue}
-                                                       onChange={this.onUpdateAttribute}
-                                                       options={[
-                                                           {
-                                                                id:"http://localhost:8090/allowedAttributes/11",
-                                                                text: "Cake Systems",
-                                                                value: "http://localhost:8090/allowedAttributes/11"
-                                                            },
-                                                             {
-                                                                id:"http://localhost:8090/allowedAttributes/12",
-                                                                text: "3DS MAC and AutoCAD",
-                                                                value: "http://localhost:8090/allowedAttributes/12"
-                                                            },
-                                                              {
-                                                                id:"http://localhost:8090/allowedAttributes/13",
-                                                                text: 'Overwatch',
-                                                                value: "http://localhost:8090/allowedAttributes/13"
-                                                            },
-                                                            {
-                                                                id:"http://localhost:8090/allowedAttributes/14",
-                                                                text: "Mobile",
-                                                                value: "http://localhost:8090/allowedAttributes/14"
-                                                            }
-                                                        ]}
-                                                       icon="glyphicon glyphicon-briefcase"
-                                                   />
-                                               </li>
-                                           </ul>
-                                       </div>
-                                       <div className="col-md-6">
-                                           <ul className="list-unstyled">
-                                               <li>
-                                                    <SelectInput
-                                                        name="OFFICE"
-                                                        label="Office"
-                                                        defaultOptionValue=""
-                                                        defaultOptionLabel="--Select Location--"
-                                                        value={this.state.allowedAttributes[2].attributeValue}
-                                                        onChange={this.onUpdateAttribute}
-                                                        options={[
-                                                            {
-                                                                id:"http://localhost:8090/allowedAttributes/5",
-                                                                text: 'Beaverton',
-                                                                value: "http://localhost:8090/allowedAttributes/5"
-                                                            },
-                                                            {
-                                                                id:"http://localhost:8090/allowedAttributes/6",
-                                                                text: "Baltimore",
-                                                                value: "http://localhost:8090/allowedAttributes/6"
-                                                            }
-                                                             ]}
-                                                        icon="glyphicon glyphicon-globe"
-                                                    />
-                                               </li>
-                                           </ul>
-                                       </div>
-                                    </div>
-                               </div>
                             </div>
-                        </div>
                     </div>
                     <div className="row">
                        <UserForm users={this.props.users} respondents={this.state.schedule.respondents} updateUsers={this.updateUsers} updateRole={this.updateRole} />
